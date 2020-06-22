ff8dbe0bcbfe760cff1574900dffa95f142f43a1
JumarySanchez <sanchezjumary@gmail.com>
2020-06-22 10:15:49 -0400

    a few updates

--- diet-back-end.js
+++ diet-back-end.js
@@ -1,18 +1,21 @@
 function healthyTips(diet){
-    if (diet === "severly underweight" && diet === "underweight"){
+    if (diet === "severley underweight" && diet === "underweight"){
         return "severley underweight or underweight? Not a problem, eating 5-6 small meals a day rather than 2 or three large meals can help boost your hunger. As part of an overall healthy diet, choose whole-grain breads, pastas and cereals; fruits and vegetables, dairy products, lean protein sources, and nuts and seeds. Remember to do a bit of strength training exercises like squats, push ups, lifting weights to help build muscles  "
     }
     else if(diet === "optimal"){
     return "Getting a good 5-10 minute workout and a healthy breakfast first thing in the morning is important and can help you maintain hunger  "
     }
     
-    else if( diet === ){
-    return  
+    else if( diet === "overweight" ){
+    return  ""
     }
     
-    else if( diet === ){
+    else if( diet === "obese" ){
         return  }
-   
+
+    else if( diet === "severley obese" ){
+            return "Contact a specialist if you are suffereing from illnesses or take medication for your weight. Cutting fast food and sugary drinks can help you lose a bit of weight" }
+
     else{
         return 'undefined'
     }
--- main.js
+++ main.js
@@ -4,26 +4,20 @@ const healthyTips = require('./color-deconstructor.js')
 const param1 = getInput(0) 
 const param2 = getInput(1)
 
-// const outPut = colorCombinator(param1,param2)
 
-
-// const color=getInput(0)
-// const outPut1= colorDeconstructor(color)
 
 
 if(param1 === undefined && param2 === undefined){
-  return  " please place eighter your height and  weight or bmi range as an input. Make sure to only put your height in inches and your weight in lbs so if you are 5 ft 2in your input should be 62 and if you are 120 lbs your input should be 120. Typing you bmi range for example undrweight, optimal or over weight will give you back a few diet and workout tips "
-  // if(param2 === 'undefines'){
-  //   return  " Please place in atleast one color"
-  // } return  " Please place in atleast one color"
+return  " please place eighter your height and  weight or bmi range as an input. Make sure to only put your height in inches and your weight in lbs so if you are 5 ft 2in your input should be 62 and if you are 120 lbs your input should be 120. Typing you bmi range for example undrweight, optimal or over weight will give you back a few diet and workout tips "
+
 }
 else if(param1 !== undefined && param2 === undefined){
-  let health = healthyTips
-  console.log(health(param1))
+let health = healthyTips
+console.log(health(param1))
 }
 else if (param1 !== undefined && param2 !== undefined){
-  let bmi = theBmiChart
-  console.log(bmi(param1,param2))
+let bmi = theBmiChart
+console.log(bmi(param1,param2))
 }
 
 
